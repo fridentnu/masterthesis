71a285aba46af2db821deca0454a1c24d4ed5a1f
Fride Nordstrand Nilsen <fridenni@cerg-bppa-home.novalocal>
2020-01-16 11:18:56 +0100

    First commit of the master

---
+++ DataExploration.R
@@ -0,0 +1,10 @@
+library(foreign)
+library(tibble)
+
+# OBS: husk at man må bruke ctrl isteden for cmd på hurtigtastene
+
+data.tilleg <- read.spss("Data/2019-12-19_108676_Data_tillegg.sav", to.data.frame=T)
+data <- read.spss("Data/2019-03-26_108676_Data Study I Cohort.sav", to.data.frame=T)
+
+glimpse(data)
+summary(data)
