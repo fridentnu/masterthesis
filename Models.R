c99a14445113aae1d7305c44e6df8d38a478c39d
Fride Nordstrand Nilsen <fridenni@stud.ntnu.no>
2020-02-14 16:46:57 +0100

    created simple linear model

--- Models.R
+++ Models.R
@@ -16,3 +16,19 @@ df.total.sc[,c(2,4,5,6,11,12,13,15)] <- scale(df.total.sc[,c(2,4,5,6,11,12,13,15
 plot_histogram(df.total.sc)
 
 describe(df.total.sc)
+
+full.pred.mod <- lm(SystolicBP3 ~ BirthYear + Sex + BMI2 + SystolicBP2 + DiastolicBP2 + 
+                      PAI2 + RecPA2 + BPHigPar2 + Smoking2 + Cholestrol2 + HDLCholestrol2 +
+                      Glucose2 + GFR2 + Creatinine2 + Education2, data=df.total.sc)
+
+fit <-ggplot(data=full.pred.mod)+
+  geom_histogram(aes(full.pred.mod$fitted.values), binwidth = 1)+
+  coord_cartesian(xlim=c(0,200), ylim=c(0,800))+
+  xlab("Predicted systolic blood pressure")
+
+res <-ggplot(data=df.total.sc)+
+  geom_histogram(aes(df.total.sc$SystolicBP3), binwidth = 1)+
+  coord_cartesian(xlim=c(0,200),ylim=c(0,800))+
+  xlab("Observed systolic blood pressure")
+grid.arrange(res,fit,nrow=2)
+
